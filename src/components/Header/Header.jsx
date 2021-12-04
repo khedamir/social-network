e3620135a81408e903739fa3885f980471548d72
khedamir <madina_a1999@mail.ru>
2021-12-04 22:45:21 +0300

    61 lesson -auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import s from './Header.module.css'
+import { NavLink } from 'react-router-dom';
 
-function Header(){
+
+function Header(props){
     return (
         <header className={s.header}>
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png' alt = 'logo'/>
+
+            <div className = {s.loginBlock}>
+                {props.isAuth ? props.login :
+                <NavLink to = {'/login'}>Login</NavLink>
+                }
+            </div>
         </header>
     );
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,5 @@
 import {combineReducers, createStore} from "redux"
+import authReducer from "./auth-reducer";
 import dialogsReducer from "./dialogs-reducer"
 import profileReducer from "./profile-reducer"
 import sidebarReducer from "./sidebar-reducer"
@@ -10,7 +11,7 @@ let reducers = combineReducers({
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-
+    auth: authReducer,
 });
 
 let store = createStore(reducers);
